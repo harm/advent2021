48f84337a6957ab8a6ebd4f134b7edc82af3c44a
Harm de Laat <harmdelaat@Harms-MacBook-Pro.local>
2021-12-02 11:55:18 +0100

    removed old comments:

--- d2/problem-d2.rb
+++ d2/problem-d2.rb
@@ -30,19 +30,3 @@ array_data.each do |row|
 end
 
 pp position_horizontal * position_depth
-
-# tupels = array_data.each_with_index.map{|item, index| array = [ array_data[index], array_data[index + 1], array_data[index + 2] ] }
-# tupels.reject! {|row| row.any? { |elem| elem.nil?  } }
-# averages_array = tupels.map{|row| row.inject(0.0) { |sum, el| sum + el.to_i } / row.size }
-
-# increases = 0
-# previous_mesurement = nil
-# averages_array.each do |line|
-# 	(increases = increases + 1) if !previous_mesurement.nil? and previous_mesurement < line
-# 	previous_mesurement = line
-# end
-
-# pp increases
-
-
-#  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 
