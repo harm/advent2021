0d0fa641465e10abfbed6fee1f4b16f5a8a46a75
Harm de Laat <harmdelaat@harms-mbp.kpn>
2021-12-07 21:47:57 +0100

    improved algorithm:

--- d7/d7.rb
+++ d7/d7.rb
@@ -1,20 +1,17 @@
 def find_solution(expensive_fuel)
 	input = File.read("input.txt").split(",").map{|v| v.to_i }
 	input = input.sort
-
-	max_position = input.max
-	min_position = input.min
+	min_position, max_position = input.minmax
 
 	fuel_cost = 0
 	fuel_costs = []
 
 	min_position.upto(max_position + 1) do |position|
-		#move all to position
 		input.each do |other_position|
-			max = [position, other_position].max
-			min = [position, other_position].min
+			min, max = [position, other_position].minmax
 			if expensive_fuel
-				(min+1).upto(max).each_with_index {|position, index| fuel_cost = fuel_cost + (index + 1) }
+				n = (max - min).abs
+				fuel_cost += n * (n + 1) / 2
 			else
 				fuel_cost = fuel_cost + (max - min)
 			end			
@@ -22,11 +19,8 @@ def find_solution(expensive_fuel)
 		fuel_costs << fuel_cost
 		fuel_cost = 0
 	end
-
 	return fuel_costs.min
 end
-
-
 
 puts "Solution problem1: #{find_solution(false)}"
 puts "Solution problem2: #{find_solution(true)}"
